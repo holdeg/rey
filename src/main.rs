9c76250f07cd2398524a41dce31ff881af15cacb
George Holderness <gholderness@microsoft.com>
2024-09-20 10:38:59 +0100

    feat: move to cgmath instead
    whoops. should have compared the available crates first, this one
    seems much more appropriate for the job!

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,13 @@
 use std::{f64::consts::PI, fs::File, io::Write};
 
-use ndarray::{arr1, Array1};
+use cgmath::{InnerSpace, Vector2, Vector3};
 use rand::Rng;
 use roots::{find_roots_quadratic, Roots};
 
-type DirectionVector = Array1<f64>;
-type WorldCoordinate = Array1<f64>;
-type TextureCoordinate = Array1<f64>;
-type Rgb = Array1<f64>;
-
-trait Normalize {
-    fn normalize(self) -> Self;
-}
-
-impl Normalize for DirectionVector {
-    fn normalize(self) -> Self {
-        let magnitude = self.dot(&self).sqrt();
-        if magnitude > 0. {
-            return self / magnitude;
-        }
-        self
-    }
-}
+type DirectionVector = Vector3<f64>;
+type WorldCoordinate = Vector3<f64>;
+type TextureCoordinate = Vector2<f64>;
+type Rgb = Vector3<f64>;
 
 trait Mix {
     fn mix(&self, other: &Self, ratio: f64) -> Self;
@@ -30,6 +16,20 @@ trait Mix {
 impl Mix for Rgb {
     fn mix(&self, other: &Self, ratio: f64) -> Self {
         self * (1. - ratio) + other * ratio
+    }
+}
+
+trait Clamp {
+    fn clamp(self, min: f64, max: f64) -> Self;
+}
+
+impl Clamp for Rgb {
+    fn clamp(self, min: f64, max: f64) -> Self {
+        Rgb::new(
+            self[0].min(max).max(min),
+            self[1].min(max).max(min),
+            self[2].min(max).max(min),
+        )
     }
 }
 
@@ -54,10 +54,10 @@ impl Interactable for Sphere {
     fn intersect(&self, ray: &Ray) -> Option<f64> {
         // Solve analytically.
 
-        let hypotenuse: DirectionVector = &ray.origin - &self.centre;
-        let a2 = ray.direction.dot(&ray.direction);
-        let a1 = 2. * ray.direction.dot(&hypotenuse);
-        let a0 = hypotenuse.dot(&hypotenuse) - self.radius.powi(2);
+        let hypotenuse: DirectionVector = ray.origin - self.centre;
+        let a2 = ray.direction.magnitude2();
+        let a1 = 2. * ray.direction.dot(hypotenuse);
+        let a0 = hypotenuse.magnitude2() - self.radius.powi(2);
 
         match find_roots_quadratic(a2, a1, a0) {
             Roots::No(_) => None,
@@ -84,11 +84,11 @@ impl Interactable for Sphere {
     }
 
     fn surface_data(&self, point_hit: WorldCoordinate) -> (DirectionVector, TextureCoordinate) {
-        let normal = (point_hit - &self.centre).normalize();
-        let tex = arr1(&[
+        let normal = (point_hit - self.centre).normalize();
+        let tex = TextureCoordinate::new(
             0.5 * (1. + normal[2].atan2(normal[0])) / PI,
             normal[1].acos() / PI,
-        ]);
+        );
         (normal, tex)
     }
 
@@ -105,9 +105,9 @@ struct Plane {
 
 impl Interactable for Plane {
     fn intersect(&self, ray: &Ray) -> Option<f64> {
-        let denominator = self.normal.dot(&ray.direction);
+        let denominator = self.normal.dot(ray.direction);
         if denominator.abs() > 1e-6 {
-            let root = (&self.point - &ray.origin).dot(&self.normal) / denominator;
+            let root = (self.point - ray.origin).dot(self.normal) / denominator;
             if root >= 0. {
                 return Some(root);
             }
@@ -116,7 +116,7 @@ impl Interactable for Plane {
         None
     }
 
-    fn surface_data(&self, point_hit: WorldCoordinate) -> (DirectionVector, TextureCoordinate) {
+    fn surface_data(&self, _point_hit: WorldCoordinate) -> (DirectionVector, TextureCoordinate) {
         todo!()
     }
 
@@ -170,20 +170,20 @@ impl RayTraceRenderer {
         pair
     }
 
-    fn cast_ray(&self, ray: Ray, scene: &Scene, recursion_depth: u8) -> Rgb {
+    fn cast_ray(&self, ray: Ray, scene: &Scene, _recursion_depth: u8) -> Rgb {
         match self.trace(&ray, scene) {
             Some((t, object)) => {
-                let point_hit: WorldCoordinate = ray.origin + &ray.direction * t;
+                let point_hit: WorldCoordinate = ray.origin + ray.direction * t;
                 let (normal, tex) = object.surface_data(point_hit);
                 let scale = 4.;
                 let pattern = ((tex[0] * scale) % 1. > 0.5) ^ ((tex[1] * scale) % 1. > 0.5);
 
-                normal.dot(&(-1. * ray.direction))
-                    * &object
+                normal.dot(-1. * ray.direction)
+                    * object
                         .colour()
                         .mix(&(object.colour() * 0.8), pattern as u8 as f64)
             }
-            None => arr1(&[0., 0., 0.]),
+            None => Rgb::new(0., 0.4, 0.8),
         }
     }
 
@@ -197,7 +197,7 @@ impl RayTraceRenderer {
         let aspect_ratio: f64 = width_f64 / height_f64;
 
         // Cast rays per pixel.
-        let mut framebuffer: Vec<Array1<f64>> = Vec::new();
+        let mut framebuffer: Vec<Rgb> = Vec::new();
         for j in 0..self.options.height {
             for i in 0..self.options.width {
                 // Translate from pixel coordinates to world coordinate system.
@@ -219,9 +219,9 @@ impl RayTraceRenderer {
                 x *= aspect_ratio * scale;
                 y *= scale;
 
-                let ray_direction: DirectionVector = arr1(&[x, y, -1.]).normalize();
+                let ray_direction: DirectionVector = DirectionVector::new(x, y, -1.).normalize();
                 let ray = Ray {
-                    origin: arr1(&[0., 0., 0.]),
+                    origin: WorldCoordinate::new(0., 0., 0.),
                     direction: ray_direction,
                 };
                 framebuffer.push(self.cast_ray(ray, &scene, 0));
@@ -233,13 +233,9 @@ impl RayTraceRenderer {
         )
         .expect("Unable to write data");
         framebuffer.into_iter().for_each(|rgb| {
-            f.write_all(
-                &(rgb.clamp(0., 1.) * 255.)
-                    .iter()
-                    .map(|channel| channel.round() as u8)
-                    .collect::<Vec<_>>(),
-            )
-            .expect("Unable to write data");
+            let pixelated = (rgb.clamp(0., 1.) * 255.).map(|channel| channel.round() as u8);
+            f.write_all(&[pixelated.x, pixelated.y, pixelated.z])
+                .expect("Unable to write data");
         });
     }
 }
@@ -253,13 +249,13 @@ fn main() {
     for _ in 0..32 {
         // 32 spheres :)
         objects.push(Box::new(Sphere {
-            centre: arr1(&[
+            centre: WorldCoordinate::new(
                 5. - 10. * rng.gen::<f64>(),
                 5. - 10. * rng.gen::<f64>(),
                 -5. - 10. * rng.gen::<f64>(),
-            ]),
+            ),
             radius: 0.5 + 0.5 * rng.gen::<f64>(),
-            base_colour: arr1(&[rng.gen(), rng.gen(), rng.gen()]),
+            base_colour: Rgb::new(rng.gen(), rng.gen(), rng.gen()),
         }));
     }
     let stage = Scene { objects };
